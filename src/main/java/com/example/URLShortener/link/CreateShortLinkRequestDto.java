e8232370ead0e879e20eb3d569f91bd36507762c
bilyardvmetro <polenovkirill@gmail.com>
2026-09-03 14:12:36 +0300

    request dto

---
+++ src/main/java/com/example/URLShortener/link/CreateShortLinkRequestDto.java
@@ -0,0 +1,11 @@
+package com.example.URLShortener.link;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateShortLinkRequestDto(
+        @NotBlank
+        @Size(max = 2048)
+        String url
+) {
+}
